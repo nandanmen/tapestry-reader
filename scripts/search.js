97a42fd4d1da3f79813babe9b0e16882326a9717
Narendra Syahrasyad <nanda.s@hey.com>
2020-08-13 18:02:59 -0700

    Move scripts into single folder

---
+++ scripts/search.js
@@ -0,0 +1,32 @@
+const mysql = require("promise-mysql");
+const unserialize = require("./unserialize");
+const fs = require("fs");
+
+const run = async (predicate) => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    port: "8889",
+    user: "root",
+    password: "root",
+    database: "ubc",
+  });
+
+  const results = await connection.query(
+    `select meta_value from wp_4_postmeta where meta_key = 'tapestry_node_data'`
+  );
+  const items = results
+    .map(({ meta_value }) => unserialize(meta_value))
+    .filter(([success, _]) => success)
+    .map(([_, node]) => node)
+    .filter(predicate);
+  connection.end();
+  return items;
+};
+
+run((node) => {
+  const url = node.typeData.mediaURL;
+  if (typeof url === "string") {
+    return url.endsWith("595");
+  }
+  return false;
+}).then((results) => console.log(results));
